54f0776c7b59b71a3ab9cf39cd85d4497acace91
Tan90Qian <tan90qian@gmail.com>
2020-01-05 20:32:35 +0800

    delete dist

--- packages/problems/dist/01/index.js
+++
@@ -1,9 +0,0 @@
-export var twoSum = function (nums, target) {
-    for (var i = 0; i < nums.length; i++) {
-        var cur = nums[i];
-        var need = target - cur;
-        var lIdx = nums.lastIndexOf(need);
-        if (lIdx !== -1 && lIdx !== i)
-            return [i, lIdx];
-    }
-};
