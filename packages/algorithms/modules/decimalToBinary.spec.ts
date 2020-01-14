ab49a3dce615b5c676506e467f86a308bfcae853
Tan90Qian <tan90qian@gmail.com>
2020-01-14 14:18:21 +0800

    feat: decimalToBinary v2

--- packages/algorithms/modules/decimalToBinary.spec.ts
+++ packages/algorithms/modules/decimalToBinary.spec.ts
@@ -1,7 +1,22 @@
 import { decimalToBinary } from "./decimalToBinary";
 
 describe("test decimalToBinary", () => {
-  it("test case 1", () => {
+  it("test base 2", () => {
+    expect(decimalToBinary(0)).toBe("");
     expect(decimalToBinary(10)).toBe("1010");
+    expect(decimalToBinary(233)).toBe("11101001");
+  });
+
+  it("test base 1", () => {
+    expect(decimalToBinary(10, 1)).toBe("");
+  });
+
+  it("test base 10", () => {
+    expect(decimalToBinary(10, 10)).toBe("10");
+    expect(decimalToBinary(233, 10)).toBe("233");
+  });
+
+  it("test base bigger than 36", () => {
+    expect(decimalToBinary(10, 37)).toBe("");
   });
 });
--- packages/algorithms/modules/decimalToBinary.ts
+++ packages/algorithms/modules/decimalToBinary.ts
@@ -1,14 +1,19 @@
 import { Stack } from "../../data-structures";
 
-export function decimalToBinary(decNumber: number) {
+export function decimalToBinary(decNumber: number, base = 2) {
   const remStack = new Stack();
   let number = decNumber;
   let rem;
   let binaryString = "";
+
+  if (base < 2 || base > 36) {
+    return "";
+  }
+
   while (number > 0) {
-    rem = Math.floor(number % 2);
+    rem = Math.floor(number % base);
     remStack.push(rem);
-    number = Math.floor(number / 2);
+    number = Math.floor(number / base);
   }
   while (!remStack.isEmpty()) {
     binaryString += remStack.pop().toString();
