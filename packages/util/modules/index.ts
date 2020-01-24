d2a6f1e671aacac96341ec2c7c7f0c3d85819a00
Tan90Qian <tan90qian@gmail.com>
2020-01-24 13:24:10 +0800

    feat: 调整util

--- packages/util/modules/index.ts
+++ packages/util/modules/index.ts
@@ -1,3 +1,7 @@
-export function defaultEquals<T = any>(a: T, b: T) {
-  return a === b;
+export interface EqualsFn<T = any> {
+  (a: T, b: T): boolean;
 }
+
+export const defaultEquals: EqualsFn = (a, b) => {
+  return a === b;
+};
